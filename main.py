e49b6389d39c0dbcfd207f8529bb138a13ae38be
Chadevenrud <87739913+Chadevenrud@users.noreply.github.com>
2021-10-13 10:01:13 -0700

    Updated Classes.  Added new method to Invoice Class

--- main.py
+++ main.py
@@ -4,10 +4,18 @@ import psycopg2 as sql
 class ClientData:
     def __init__(self, username, password):
         try:
-            con = sql.connect(f"dbname=MCE_Clients user={username} password=pass{password}")
+            self.con = sql.connect(f"dbname=MCE_Clients user={username} password={password}")
         except:
-            return print("User name or password does not match MCE_Clients Database")
+            print("User name or password does not match MCE_Clients Database")
 
+
+    def client_tables(self):
+        commands = """
+        CREATE TABLE Clients ( ClientID SERIAL PRIMARY KEY, ClientName VARCHAR(255), Address1 VARCHAR (255), Address2 VARCHAR (255),
+            City VARCHAR (200), State VARCHAR (2), ZIP VARCHAR (20))          
+        """
+        self.cursor = self.con.cursor()
+        self.cursor.execute(commands)
 
 
 class Clients:
@@ -17,25 +25,43 @@ class Clients:
         self.city = city
         self.state = state
 
-
-def invoice_amount(hours, rate):
-    amount = float(hours) * float(rate)
-    return amount
-
-
-def work_description():
-    description = input("Enter Work Description: ")
-    return description
+    def client_contact(self, contact_name, contact_email, contact_phone):
+        pass
 
 
 class Invoice:
-    def __init__(self, invoice_number, date):
+
+    def __init__(self, invoice_number, client, date):
         self.invoice_number = invoice_number
         self.date = date
 
+    @staticmethod
+    def invoice_detail():
+        item_list = {}
+        amount_list = []
+        while True:
+            line_number = input("Enter Line Number: ")
+            if line_number == '' or line_number == 'Done'.lower():
+                break
+            else:
+                hours = input("Enter hours: ")
+                rate = input("Enter rate: ")
+                work_description = input("Work description: ")
+                amount = float(hours) * float(rate)
+                amount_list.append(amount)
+                item_list[line_number] = [
+                    {"Description": work_description, "Rate": rate, "Hours": hours, "line_amount": amount}]
+        return item_list
 
-class CreateInvoice:
+            
+
+
+    # @staticmethod
+    # def work_description():
+    #     description = input("Enter Work Description: ")
+    #     return description
+
+
+class CreateInvoice(Invoice):
     pass
-
-
 
