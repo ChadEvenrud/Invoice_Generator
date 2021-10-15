281e43409eba69083c0e730f84003a745201805d
Chadevenrud <87739913+Chadevenrud@users.noreply.github.com>
2021-10-14 19:23:43 -0700

    Updated Client Database Class to create new database tables.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import psycopg2 as sql
 
-
+# Connects to the PostgreSQL database.  The class will also add Tables for the application.
 class ClientData:
     def __init__(self, username, password):
         try:
@@ -10,13 +10,17 @@ class ClientData:
 
     def client_tables(self):
         commands = """
-        CREATE TABLE Clients ( ClientID SERIAL PRIMARY KEY, ClientName VARCHAR(255), Address1 VARCHAR (255), Address2 VARCHAR (255),
+        CREATE TABLE IF NOT EXISTS Clients ( ClientID SERIAL PRIMARY KEY, ClientName VARCHAR(255), Address1 VARCHAR (255), Address2 VARCHAR (255),
             City VARCHAR (200), State VARCHAR (2), ZIP VARCHAR (20))          
         """
         self.cursor = self.con.cursor()
         self.cursor.execute(commands)
+        self.con.commit()
+        self.cursor.close()
+        self.con.close()
 
 
+# Adds new Client Records to the database
 class Clients:
     def __init__(self, client_name, address, city, state):
         self.client = client_name
@@ -38,6 +42,7 @@ class Invoice:
     def invoice_detail(self):
         item_list = {}
         amount_list = []
+
         while True:
             line_number = input("Enter Line Number: ")
             if line_number == '' or line_number == 'Done'.lower():
@@ -52,13 +57,17 @@ class Invoice:
                     {"Description": work_description, "Rate": rate, "Hours": hours, "line_amount": amount}]
         for x in amount_list:
             self.invoice_amount += x
+        for x in item_list.items():
+            print(x[0], x[1][0]["Description"], x[1][0]["Rate"], x[1][0]["Hours"], x[1][0]["line_amount"])
 
 
 class CreateInvoice(Invoice):
     pass
 
 
-test = Invoice("0001", "PBG", "01/01/2020")
-test.invoice_detail()
-print(test.invoice_amount)
+test = ClientData("chad_evenrud", "Waterpolo1!")
+test.client_tables()
 
+# test = Invoice("0001", "PBG", "01/01/2020")
+# test.invoice_detail()
+# print(test.invoice_amount)
